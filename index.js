d9089af75869f1f80c4e7a7258ffa181d5bfa1b3
Rafael Quines <rafael.quines@totvs.com.br>
2018-02-05 10:28:14 -0200

    fix env vars

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var winston = require('winston');
-require('./winston-mqtt-transport').MqttTransport;
+require('./lib/winston-mqtt-transport').MqttTransport;
 require('dotenv').config();
 
-if (!process.env.LOG_FILENAME) {
+if (!process.env.LOG_OUTPUT || (process.env.LOG_OUTPUT != 'file' && process.env.LOG_OUTPUT != 'mqtt' &&
+        process.env.LOG_OUTPUT != 'both')) {
     console.error('Invalid envoriment');
-    console.error('Please set LOG_FILENAME var');
+    console.error('Set LOG_OUTPUT to file, mqtt or both');
+    process.exit(1);
+}
+
+if ((process.env.LOG_OUTPUT === 'mqtt' || process.env.LOG_OUTPUT === 'both') &&
+    (!process.env.MQTT_URL || !process.env.MQTT_TOPIC)) {
+    console.error('Invalid envoriment');
+    console.error('Set MQTT_URL and MQTT_TOPIC');
+    process.exit(1);
+}
+
+if ((process.env.LOG_OUTPUT === 'file' || process.env.LOG_OUTPUT === 'both') &&
+    !process.env.LOG_FILENAME) {
+    console.error('Invalid envoriment');
+    console.error('Set LOG_FILENAME');
     process.exit(1);
 }
 
@@ -12,22 +27,25 @@ process.env.GENERATION_TIME = process.env.GENERATION_TIME || 1000;
 
 const logger = new winston.Logger();
 
-logger.add(winston.transports.MqttTransport, {
-    name: 'mqtt-source',
-    topic: process.env.MQTT_TOPIC,
-    host: process.env.MQTT_URL
-});
+if (process.env.LOG_OUTPUT === 'mqtt' || process.env.LOG_OUTPUT === 'both') {
+    logger.add(winston.transports.MqttTransport, {
+        name: 'mqtt-source',
+        topic: process.env.MQTT_TOPIC,
+        host: process.env.MQTT_URL
+    });
+}
 
-
-logger.add(winston.transports.File, {
-    filename: process.env.LOG_FILENAME,
-    timestamp: true,
-    json: false
-});
+if (process.env.LOG_OUTPUT === 'file' || process.env.LOG_OUTPUT === 'both') {
+    logger.add(winston.transports.File, {
+        filename: process.env.LOG_FILENAME,
+        timestamp: true,
+        json: false
+    });
+}
 
 var cont = 0;
 
 setInterval(() => {
     console.log('Generating ' + ++cont);
-    logger.info('filelog-generator: ' + cont);
+    logger.info('log-generator: ' + cont);
 }, process.env.GENERATION_TIME);
